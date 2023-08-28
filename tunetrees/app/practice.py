ff8433fdeb23cf94e3635ce931118aebcb4b5e49
Scott Boag <scott_boag@us.ibm.com>
2023-08-28 18:20:37 -0400

    set list for recently played to 25 for now

--- tunetrees/app/practice.py
+++ tunetrees/app/practice.py
@@ -4,7 +4,10 @@ from typing import List
 from jinja2 import Environment, FileSystemLoader
 
 from tunetrees.app.database import SessionLocal
-from tunetrees.app.queries import get_practice_list_recently_played, get_practice_list_scheduled
+from tunetrees.app.queries import (
+    get_practice_list_recently_played,
+    get_practice_list_scheduled,
+)
 from tunetrees.models.tunetrees import Tune
 
 
@@ -13,7 +16,9 @@ async def render_practice_page() -> str:
     try:
         db = SessionLocal()
         tunes_scheduled: List[Tune] = get_practice_list_scheduled(db, limit=10)
-        tunes_recently_played: List[Tune] = get_practice_list_recently_played(db, limit=10)
+        tunes_recently_played: List[Tune] = get_practice_list_recently_played(
+            db, limit=25
+        )
         tunetrees_package_top = Path(__file__).parent.parent
         assert tunetrees_package_top.exists()
         templates_folder = tunetrees_package_top.joinpath("templates")
