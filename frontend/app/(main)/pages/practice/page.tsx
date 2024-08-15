b20c84d527f75bb557090eb728683c1ff7044888
Scott Boag <sboag@gmail.com>
2024-08-15 17:58:00 -0400

    ♻️ 🚧 Refactor tab structure to use shadcn-ui (task 2 of issue #49)

--- frontend/app/(main)/pages/practice/page.tsx
+++ frontend/app/(main)/pages/practice/page.tsx
@@ -1,27 +1,38 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { getPracticeListScheduled, getRecentlyPracticed } from "./queries";
+
 import { Tune } from "./types";
-import { Box, CircularProgress, Tab, Tabs, Typography } from "@mui/material";
 
 import ScheduledTunesGrid from "./components/ScheduledTunesGrid";
 import ScheduledTunesGridOld from "./components/ScheduledTunesGridOld";
 import RecentlyPracticed from "./components/RecentlyPracticed";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
+function CircularProgress() {
+  return (
+    <div className="flex justify-center items-center h-32">
+      <div className="w-12 h-12 border-t-2 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+    </div>
+  );
 }
 
-type WarmupState = {
-  x: number;
-};
-
 export default function Practice() {
-  // let [warmup, setWarmup] = useState<WarmupState>()
   let [scheduled, setScheduled] = useState<Tune[]>();
   let [recentlyPracticed, setRecentlyPracticed] = useState<Tune[]>();
+  // const [progress, setProgress] = React.useState(13);
 
   useEffect(() => {
     const getScheduled = async () => {
@@ -39,72 +50,68 @@ export default function Practice() {
     getRecent();
   }, []);
 
-  function CustomTabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography component={"div"}>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-
-  const [tabValue, setTabValue] = React.useState(0);
-
-  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
-    setTabValue(newValue);
-  };
-
   return (
-    //   {/* <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mx: 4 }}>
-    //   <h1 className={josefinSans.className}>tunetrees</h1>
-    //   <WarmUpTimer />
-    // </Box> */}
-    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-      <Tabs
-        value={tabValue}
-        onChange={handleTabChange}
-        aria-label="basic tabs example"
-      >
-        {/* <Tab label="Warmup" /> */}
-        <Tab label="Scheduled for Practice" />
-        <Tab label="Scheduled for Practice (New Grid)" />
-        <Tab label="Recently Practiced" />
-      </Tabs>
-      <CustomTabPanel value={tabValue} index={0}>
-        {scheduled ? (
-          <ScheduledTunesGrid tunes={scheduled} />
-        ) : (
-          <CircularProgress />
-        )}
-      </CustomTabPanel>
-      <CustomTabPanel value={tabValue} index={1}>
-        {scheduled ? (
-          <ScheduledTunesGridOld tunes={scheduled} />
-        ) : (
-          <CircularProgress />
-        )}
-      </CustomTabPanel>
-      <CustomTabPanel value={tabValue} index={2}>
-        {recentlyPracticed ? (
-          <RecentlyPracticed tunes={recentlyPracticed} />
-        ) : (
-          <CircularProgress />
-        )}
-      </CustomTabPanel>
-    </Box>
-    //   {/* <CustomTabPanel value={tabValue} index={0}>
-    //   {warmup ? <WarmUpTimer /> : <CircularProgress />}
-    // </CustomTabPanel> */}
+    <Tabs defaultValue="scheduled" className="flex h-full w-full flex-col">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="scheduled">Scheduled for Practice</TabsTrigger>
+        <TabsTrigger value="repertoire">Repertoire</TabsTrigger>
+        {/* <TabsTrigger value="xxx">Material-ui Grid</TabsTrigger> */}
+      </TabsList>
+      <TabsContent value="scheduled">
+        <Card>
+          {/* <CardHeader>
+            <CardTitle>Scheduled for Practice</CardTitle>
+            <CardDescription>Daily Review.</CardDescription>
+          </CardHeader> */}
+          <CardContent className="space-y-2">
+            {scheduled ? (
+              <ScheduledTunesGrid tunes={scheduled} />
+            ) : (
+              <CircularProgress />
+            )}
+          </CardContent>
+          {/* <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter> */}
+        </Card>
+      </TabsContent>
+      <TabsContent value="repertoire">
+        <Card>
+          {/* <CardHeader>
+            <CardTitle>Repertoire</CardTitle>
+            <CardDescription>Overall repertoire.</CardDescription>
+          </CardHeader> */}
+          <CardContent className="space-y-2">
+            {recentlyPracticed ? (
+              <RecentlyPracticed tunes={recentlyPracticed} />
+            ) : (
+              <CircularProgress />
+            )}
+          </CardContent>
+          {/* <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter> */}
+        </Card>
+      </TabsContent>
+
+      <TabsContent value="xxx">
+        <Card>
+          {/* <CardHeader>
+            <CardTitle>Scheduled for Practice (Material-ui Grid)</CardTitle>
+            <CardDescription>Older grid for reference.</CardDescription>
+          </CardHeader> */}
+          <CardContent className="space-y-2">
+            {scheduled ? (
+              <ScheduledTunesGridOld tunes={scheduled} />
+            ) : (
+              <CircularProgress />
+            )}
+          </CardContent>
+          {/* <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter> */}
+        </Card>
+      </TabsContent>
+    </Tabs>
   );
 }
---
+++ frontend/app/(main)/pages/practice/page_old.tsx
@@ -0,0 +1,110 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { getPracticeListScheduled, getRecentlyPracticed } from "./queries";
+import { Tune } from "./types";
+import { Box, CircularProgress, Tab, Tabs, Typography } from "@mui/material";
+
+import ScheduledTunesGrid from "./components/ScheduledTunesGrid";
+import ScheduledTunesGridOld from "./components/ScheduledTunesGridOld";
+import RecentlyPracticed from "./components/RecentlyPracticed";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+type WarmupState = {
+  x: number;
+};
+
+export default function Practice() {
+  // let [warmup, setWarmup] = useState<WarmupState>()
+  let [scheduled, setScheduled] = useState<Tune[]>();
+  let [recentlyPracticed, setRecentlyPracticed] = useState<Tune[]>();
+
+  useEffect(() => {
+    const getScheduled = async () => {
+      const data = await getPracticeListScheduled();
+      setScheduled(data);
+    };
+    getScheduled();
+  }, []);
+
+  useEffect(() => {
+    const getRecent = async () => {
+      const data = await getRecentlyPracticed();
+      setRecentlyPracticed(data);
+    };
+    getRecent();
+  }, []);
+
+  function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography component={"div"}>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
+
+  return (
+    //   {/* <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mx: 4 }}>
+    //   <h1 className={josefinSans.className}>tunetrees</h1>
+    //   <WarmUpTimer />
+    // </Box> */}
+    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+      <Tabs
+        value={tabValue}
+        onChange={handleTabChange}
+        aria-label="basic tabs example"
+      >
+        {/* <Tab label="Warmup" /> */}
+        <Tab label="Scheduled for Practice" />
+        <Tab label="Scheduled for Practice (New Grid)" />
+        <Tab label="Recently Practiced" />
+      </Tabs>
+      <CustomTabPanel value={tabValue} index={0}>
+        {scheduled ? (
+          <ScheduledTunesGrid tunes={scheduled} />
+        ) : (
+          <CircularProgress />
+        )}
+      </CustomTabPanel>
+      <CustomTabPanel value={tabValue} index={1}>
+        {scheduled ? (
+          <ScheduledTunesGridOld tunes={scheduled} />
+        ) : (
+          <CircularProgress />
+        )}
+      </CustomTabPanel>
+      <CustomTabPanel value={tabValue} index={2}>
+        {recentlyPracticed ? (
+          <RecentlyPracticed tunes={recentlyPracticed} />
+        ) : (
+          <CircularProgress />
+        )}
+      </CustomTabPanel>
+    </Box>
+    //   {/* <CustomTabPanel value={tabValue} index={0}>
+    //   {warmup ? <WarmUpTimer /> : <CircularProgress />}
+    // </CustomTabPanel> */}
+  );
+}
