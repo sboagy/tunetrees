d7f0a0b5433f9b3452dcdbcc7774de66fd5f6ffa
Scott Boag <sboag@gmail.com>
2024-08-18 21:37:12 -0400

    🚧 🗃️

--- tunetrees/api/mappers/tunes_mapper.py
+++ tunetrees/api/mappers/tunes_mapper.py
@@ -1,23 +1,30 @@
-from tunetrees.models.tunetrees import Tune
+from typing import Any, List
+
+from sqlalchemy.engine.row import Row
+from tunetrees.models.practice_list_joined import (
+    practice_list_columns,
+)
 
 
-def tunes_mapper(tune: Tune):
+def tunes_mapper(tune: Row[Any]) -> dict[str, Any]:
     tune_dict = {}
-    tune_dict["id"] = tune[0]
-    tune_dict["title"] = tune[1]
-    tune_dict["type"] = tune[2]
-    tune_dict["structure"] = tune[3]
-    tune_dict["mode"] = tune[4]
-    tune_dict["incipit"] = tune[5]
-    tune_dict["learned"] = tune[6]
-    tune_dict["practiced"] = tune[7]
-    tune_dict["quality"] = tune[8]
-    tune_dict["easiness"] = tune[9]
-    tune_dict["interval"] = tune[10]
-    tune_dict["repetitions"] = tune[11]
-    tune_dict["review_date"] = tune[12]
-    tune_dict["backup_practiced"] = tune[13]
-    tune_dict["notes_private"] = tune[14]
-    tune_dict["notes_public"] = tune[15]
-    tune_dict["tags"] = tune[16]
+    tune_dict["id"] = tune[practice_list_columns["TuneID"]]
+    tune_dict["title"] = tune[practice_list_columns["TuneTitle"]]
+    tune_dict["type"] = tune[practice_list_columns["TuneType"]]
+    tune_dict["structure"] = tune[practice_list_columns["TuneStructure"]]
+    tune_dict["mode"] = tune[practice_list_columns["TuneMode"]]
+    tune_dict["incipit"] = tune[practice_list_columns["TuneIncipit"]]
+    tune_dict["learned"] = tune[practice_list_columns["PlaylistLearned"]]
+    tune_dict["practiced"] = tune[practice_list_columns["PracticeDate"]]
+    tune_dict["quality"] = tune[practice_list_columns["PracticeQuality"]]
+    tune_dict["easiness"] = tune[practice_list_columns["PracticeEasiness"]]
+    tune_dict["interval"] = tune[practice_list_columns["PracticeInterval"]]
+    tune_dict["repetitions"] = tune[practice_list_columns["PracticeRepetitions"]]
+    tune_dict["review_date"] = tune[practice_list_columns["PracticeReviewDate"]]
+    tune_dict["backup_practiced"] = tune[
+        practice_list_columns["PracticeBackupPracticed"]
+    ]
+    tune_dict["notes_private"] = tune[practice_list_columns["AnnotationNotePrivate"]]
+    tune_dict["notes_public"] = tune[practice_list_columns["AnnotationNotePublic"]]
+    tune_dict["tags"] = tune[practice_list_columns["AnnotationTags"]]
     return tune_dict
--- tunetrees/app/queries.py
+++ tunetrees/app/queries.py
@@ -1,15 +1,19 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import Any, Dict, List, Optional
 
 from sqlalchemy import func
 from sqlalchemy.orm import Query, Session
+from sqlalchemy.engine.row import Row
 from tabulate import tabulate
 
 from tunetrees.app.database import SessionLocal
+from tunetrees.models.practice_list_joined import (
+    get_practice_list_query,
+    practice_list_columns,
+)
 from tunetrees.models.tunetrees import (
     PracticeRecord,
     Tune,
-    t_practice_list_joined,
 )
 
 
@@ -48,39 +52,41 @@ def get_practice_record_table(
 
 
 def get_most_recent_review_date(db: Session) -> datetime:
-    query: Query[Any] = db.query(t_practice_list_joined)
-    most_recent_schedualed = query.order_by(
-        func.DATE(t_practice_list_joined.columns.get("Practiced")).desc()
-    ).limit(1)
+    # query: Query[Any] = db.query(t_practice_list_joined)
+    # most_recent_schedualed = query.order_by(
+    #     func.DATE(t_practice_list_joined.columns.get("Practiced")).desc()
+    # ).limit(1)
 
-    review_date_column_name = "ReviewDate"
-    review_date = t_practice_list_joined.columns.get(review_date_column_name)
-    assert review_date is not None
-    review_date_column_index = list(t_practice_list_joined.columns.keys()).index(
-        review_date_column_name
-    )
-    most_recent_review_date_str = most_recent_schedualed[0][review_date_column_index]
-    most_recent_review_date = datetime.fromisoformat(most_recent_review_date_str)
-    return most_recent_review_date
+    # review_date_column_name = "ReviewDate"
+    # review_date = t_practice_list_joined.columns.get(review_date_column_name)
+    # assert review_date is not None
+    # review_date_column_index = list(t_practice_list_joined.columns.keys()).index(
+    #     review_date_column_name
+    # )
+    # most_recent_review_date_str = most_recent_schedualed[0][review_date_column_index]
+    # most_recent_review_date = datetime.fromisoformat(most_recent_review_date_str)
+    # return most_recent_review_date
+    return datetime.today()
 
 
 def get_most_recent_practice_date(db: Session) -> datetime:
-    query: Query[Any] = db.query(t_practice_list_joined)
-    most_recent_schedualed = query.order_by(
-        func.DATE(t_practice_list_joined.columns.get("Practiced")).desc()
-    ).limit(1)
+    # query: Query[Any] = db.query(t_practice_list_joined)
+    # most_recent_schedualed = query.order_by(
+    #     func.DATE(t_practice_list_joined.columns.get("Practiced")).desc()
+    # ).limit(1)
 
-    practice_date_column_name = "Practiced"
-    practice_date = t_practice_list_joined.columns.get(practice_date_column_name)
-    assert practice_date is not None
-    practice_date_column_index = list(t_practice_list_joined.columns.keys()).index(
-        practice_date_column_name
-    )
-    most_recent_practice_date_str = most_recent_schedualed[0][
-        practice_date_column_index
-    ]
-    most_recent_practice_date = datetime.fromisoformat(most_recent_practice_date_str)
-    return most_recent_practice_date
+    # practice_date_column_name = "Practiced"
+    # practice_date = t_practice_list_joined.columns.get(practice_date_column_name)
+    # assert practice_date is not None
+    # practice_date_column_index = list(t_practice_list_joined.columns.keys()).index(
+    #     practice_date_column_name
+    # )
+    # most_recent_practice_date_str = most_recent_schedualed[0][
+    #     practice_date_column_index
+    # ]
+    # most_recent_practice_date = datetime.fromisoformat(most_recent_practice_date_str)
+    # return most_recent_practice_date
+    return datetime.today()
 
 
 def get_practice_list_scheduled(
@@ -90,7 +96,7 @@ def get_practice_list_scheduled(
     print_table=False,
     review_sitdown_date: Optional[datetime] = None,
     acceptable_delinquency_window=7,
-) -> List[Tune]:
+) -> List[Row[Any]]:
     """Get a list of tunes to practice on the review_sitdown_date.
 
     Get all tunes scheduled between the acceptable_delinquency_window and review_sitdown_date, but limit number to the `limit` var.
@@ -110,36 +116,32 @@ def get_practice_list_scheduled(
         review_sitdown_date = datetime.today()
     assert isinstance(review_sitdown_date, datetime)
 
-    query: Query[Any] = db.query(t_practice_list_joined)
-    scheduled_rows: List[Tune] = (
-        query.filter(
-            func.DATE(t_practice_list_joined.columns.get("ReviewDate"))
-            > review_sitdown_date - timedelta(acceptable_delinquency_window)
-        )
-        .filter(
-            func.DATE(t_practice_list_joined.columns.get("ReviewDate"))
-            <= review_sitdown_date
-        )
-        .order_by(func.DATE(t_practice_list_joined.columns.get("ReviewDate")).desc())
+    practice_list_query = get_practice_list_query(db, playlist_ref=1, user_ref=1)
+
+    scheduled_rows: List[Row] = (
+        practice_list_query.where(PracticeRecord.ReviewDate <= review_sitdown_date)
+        .order_by(func.DATE(PracticeRecord.ReviewDate).desc())
         .offset(skip)
         .limit(limit)
         .all()
     )
+
+    rows = scheduled_rows
     # aged_limit = limit-len(scheduled_rows)
     # if aged_limit <= 0:
     #     aged_limit = 2
-    aged_limit = 2
-    aged_rows: List[Tune] = (
-        query.order_by(func.DATE(t_practice_list_joined.columns.get("Practiced")).asc())
-        .offset(skip)
-        .limit(aged_limit)
-        .all()
-    )
-    rows = scheduled_rows + aged_rows
+    # aged_limit = 2
+    # aged_rows: List[Tune] = (
+    #     query.order_by(func.DATE(t_practice_list_joined.columns.get("Practiced")).asc())
+    #     .offset(skip)
+    #     .limit(aged_limit)
+    #     .all()
+    # )
+    # rows = scheduled_rows + aged_rows
 
-    if print_table:
-        print("\n--------")
-        print(tabulate(rows, headers=t_practice_list_joined.columns.keys()))
+    # if print_table:
+    #     print("\n--------")
+    #     print(tabulate(rows, headers=t_practice_list_joined.columns.keys()))
 
     return rows
 
@@ -147,29 +149,36 @@ def get_practice_list_scheduled(
 def get_practice_list_recently_played(
     db: Session, skip: int = 0, limit: int = 100, print_table=False
 ) -> List[Tune]:
-    query: Query[Any] = db.query(t_practice_list_joined)
-    rows: List[Tune] = (
-        query.order_by(
-            func.DATE(t_practice_list_joined.columns.get("Practiced")).desc()
-        )
-        .offset(skip)
-        .limit(limit)
-        .all()
-    )
+    # query: Query[Any] = db.query(t_practice_list_joined)
+    # rows: List[Tune] = (
+    #     query.order_by(
+    #         func.DATE(t_practice_list_joined.columns.get("Practiced")).desc()
+    #     )
+    #     .offset(skip)
+    #     .limit(limit)
+    #     .all()
+    # )
 
-    if print_table:
-        print("\n--------")
-        print(tabulate(rows, headers=t_practice_list_joined.columns.keys()))
+    # if print_table:
+    #     print("\n--------")
+    #     print(tabulate(rows, headers=t_practice_list_joined.columns.keys()))
 
-    return rows
+    # return rows
+    return []
 
 
 def _run_experiment():
     db = None
     try:
         db = SessionLocal()
-        tunes: List[Tune] = get_practice_list_scheduled(db, limit=10, print_table=True)
-        assert tunes
+        tunes = get_practice_list_scheduled(db, limit=10, print_table=True)
+        for tune in tunes:
+            for column_name in practice_list_columns:
+                column_index = practice_list_columns[column_name]
+                if column_index != 0:
+                    print("   ", end="")
+                print(f"{column_name}: {tune[column_index]}")
+                # assert tunes
     finally:
         if db:
             db.close()
