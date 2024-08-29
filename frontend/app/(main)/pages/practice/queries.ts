b5ad1f33d9427930d5de1adbb00d930ac740eb04
Scott Boag <sboag@gmail.com>
2024-08-29 10:22:54 -0400

    ♻️ 🥅  Handle errors in getPracticeListScheduled and getRecentlyPracticed functions such that a dummy entry is provided to be visible to the UI, and refactor code so it's well typed

--- frontend/app/(main)/pages/practice/queries.ts
+++ frontend/app/(main)/pages/practice/queries.ts
@@ -1,44 +1,69 @@
+"use server";
+
 import axios from "axios";
+import type { Tune } from "./types";
 
 const client = axios.create({
   // baseURL: process.env.TT_API_BASE_URL
   baseURL: process.env.NEXT_PUBLIC_TT_BASE_URL,
-  // baseURL: "http://tunetrees.com:3000"
-  // baseURL: "http://server:8000/tunetrees"
-  // baseURL: "http://localhost:8000/tunetrees"
-  // baseURL: "http://tunetrees.com:8000/tunetrees"
 });
 
-//Possibly need a mapper here too! Not sure what the convention is for things like variable case when moving between languages
-export const getPracticeListScheduled = async (
+const ERROR_TUNE: Tune[] = [
+  {
+    id: 0,
+    title: "Error",
+    type: "Error",
+    structure: "Error",
+    mode: "Error",
+    incipit: "Error",
+    learned: "Error",
+    practiced: "Error",
+    quality: "Error",
+    easiness: 0,
+    interval: 0,
+    repetitions: 0,
+    review_date: "Error",
+    backup_practiced: "Error",
+    notes_private: "Error", // Optional property, should't need to be set
+    notes_public: "Error", // Optional property, should't need to be set
+    tags: "Error", // Optional property, should't need to be set
+    recallEval: "Error", // Optional property, should't need to be set
+  },
+];
+
+export async function getPracticeListScheduled(
   user_id: string,
   playlist_id: string,
-) => {
+): Promise<Tune[]> {
   try {
+    console.log("Environment Variables:", process.env);
     console.log("In getPracticeListScheduled: baseURL: %s", client.getUri());
     console.log("user_id: %s, playlist_id: %s", user_id, playlist_id);
-    const response = await client.get(
+    const response = await client.get<Tune[]>(
       `/get_practice_list_scheduled/${user_id}/${playlist_id}`,
     );
-    const data = await response.data;
-    // console.log('data in query', data)
+    const data = response.data;
     return data;
   } catch (e) {
-    console.log(console.error());
+    console.error("Error in getPracticeListScheduled: ", e);
+    // Return a dummy Tune object to avoid breaking the UI
+    return Promise.resolve<Tune[]>(ERROR_TUNE);
   }
-};
+}
 
-export const getRecentlyPracticed = async (
+export async function getRecentlyPracticed(
   user_id: string,
   playlist_id: string,
-) => {
+): Promise<Tune[]> {
   try {
-    const response = await client.get(
+    const response = await client.get<Tune[]>(
       `/get_tunes_recently_played/${user_id}/${playlist_id}`,
     );
-    const data = await response.data;
+    const data = response.data;
     return data;
   } catch (e) {
-    console.log(console.error());
+    console.error("Error in getRecentlyPracticed: ", e);
+    // Return a dummy Tune object to avoid breaking the UI
+    return Promise.resolve<Tune[]>(ERROR_TUNE);
   }
-};
+}
