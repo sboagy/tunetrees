ea0280cc317506532fc37cfe0689179a0107ee38
Scott Boag <sboag@gmail.com>
2024-10-22 17:59:51 -0400

    🚸 🐛  Make it so the footer doesn't wrap, and make it be iffy collapsable.  Closes #80.
    Closes: Bug: footer takes too much space on small device #80
    But, note I may want to do more on the small device once I test there.

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -2,37 +2,43 @@
 import CustomLink from "./CustomLink";
 import { Toaster } from "./ui/toaster";
 
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+
 export default function Footer() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <footer className="flex flex-col gap-4 px-4 my-4 mx-0 w-full text-sm sm:flex-row sm:justify-between sm:items-center sm:px-6 sm:my-12 sm:mx-auto sm:max-w-8xl sm:h-5">
-      <div className="flex flex-col gap-4 sm:flex-row">
-        {/* <CustomLink href="https://nextjs.authjs.dev">Documentation</CustomLink> */}
-        {/* <CustomLink href="https://www.npmjs.com/package/next-auth">
-          NPM
-        </CustomLink> */}
-        <CustomLink href="https://github.com/sboagy/tunetrees/blob/main/docs/core-proposal.md">
-          Whitepaper
-        </CustomLink>
-        <CustomLink href="https://github.com/sboagy/tunetrees">
-          Source on GitHub
-        </CustomLink>
-        <CustomLink href="https://github.com/sboagy/tunetrees/blob/main/docs/sr_readme.md">
-          Spaced Repetition Terminology
-        </CustomLink>
-        <CustomLink href="mailto:sboag@tunetrees.com">Contact</CustomLink>
-        <CustomLink href="/policy">Policy</CustomLink>
-        <Toaster />
-      </div>
-      {/* <div className="flex gap-2 justify-start items-center">
-        <img
-          className="size-5"
-          src="https://authjs.dev/img/logo-sm.png"
-          alt="Auth.js Logo"
-        />
-        <CustomLink href="https://npmjs.org/package/next-auth">
-          {packageJSON.version}
-        </CustomLink>
-      </div> */}
+    <footer>
+      <Collapsible.Root
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className={isOpen ? "" : "h-4"}
+      >
+        <div className="flex items-center  space-x-4">
+          <Collapsible.Trigger asChild>
+            {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          </Collapsible.Trigger>
+          <Collapsible.Content className="collapsible-content">
+            {/* <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:whitespace-nowrap sm:overflow-x-clip"> */}
+            <div className="flex-col whitespace-nowrap space-x-4">
+              <CustomLink href="https://github.com/sboagy/tunetrees/blob/main/docs/core-proposal.md">
+                Whitepaper
+              </CustomLink>
+              <CustomLink href="https://github.com/sboagy/tunetrees">
+                Source on GitHub
+              </CustomLink>
+              <CustomLink href="https://github.com/sboagy/tunetrees/blob/main/docs/sr_readme.md">
+                Spaced Repetition Terminology
+              </CustomLink>
+              <CustomLink href="mailto:sboag@tunetrees.com">Contact</CustomLink>
+              <CustomLink href="/policy">Policy</CustomLink>
+              <Toaster />
+            </div>
+          </Collapsible.Content>
+        </div>
+      </Collapsible.Root>
     </footer>
   );
 }
