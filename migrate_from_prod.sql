8596aa449d90fa473c5df214fb4fc0939a79537d
Scott Boag <sboag@gmail.com>
2024-10-29 22:01:08 -0400

    🔨  script changes to target snake case consistent field names, and integer consistent IDs and IDREFS

--- migrate_from_prod.sql
+++ migrate_from_prod.sql
@@ -1,10 +1,10 @@
 -- Attach the source database
--- ATTACH DATABASE '/Users/sboag/gittt/tunetrees/backup_practice_sep_20.sqlite' AS source_db;
+ATTACH DATABASE 'tunetrees_do.sqlite3' AS source_db;
 PRAGMA foreign_keys = ON;
-ATTACH DATABASE :source_db AS source_db;
+-- ATTACH DATABASE :source_db AS source_db;
 
 -- Tune Table Migration
-INSERT OR REPLACE INTO main.tune (ID, Type, Structure, Title, Mode, Incipit)
+INSERT OR REPLACE INTO main.tune (id, type, structure, title, mode, incipit)
 SELECT ID, Type, Structure, Title, Mode, Incipit
 FROM source_db.tune;
 
@@ -14,23 +14,23 @@ FROM source_db.tune;
 -- FROM source_db.user;
 
 -- Playlist Table Migration
-INSERT OR REPLACE INTO main.playlist (PLAYLIST_ID, USER_REF, instrument)
+INSERT OR REPLACE INTO main.playlist (playlist_id, user_ref, instrument)
 SELECT PLAYLIST_ID, USER_REF, instrument
 FROM source_db.playlist;
 
 -- Playlist_Tune Table Migration
-INSERT OR REPLACE INTO main.playlist_tune (PLAYLIST_REF, TUNE_REF, Current, Learned)
-SELECT PLAYLIST_REF, TUNE_REF, Current, Learned
+INSERT OR REPLACE INTO main.playlist_tune (playlist_ref, tune_ref, current, learned)
+SELECT PLAYLIST_REF, CAST(TUNE_REF AS INTEGER), Current, Learned
 FROM source_db.playlist_tune;
 
 -- Practice_Record Table Migration
-INSERT OR REPLACE INTO main.practice_record (PLAYLIST_REF, TUNE_REF, Practiced, Quality, ID, Easiness, Interval, Repetitions, ReviewDate, BackupPracticed)
-SELECT PLAYLIST_REF, TUNE_REF, Practiced, Quality, ID, Easiness, Interval, Repetitions, ReviewDate, BackupPracticed
+INSERT OR REPLACE INTO main.practice_record (playlist_ref, tune_ref, practiced, quality, id, easiness, interval, repetitions, review_date, backup_practiced)
+SELECT PLAYLIST_REF, CAST(TUNE_REF AS INTEGER), Practiced, Quality, ID, Easiness, Interval, Repetitions, ReviewDate, BackupPracticed
 FROM source_db.practice_record;
 
 -- User_Annotation_Set Table Migration
-INSERT OR REPLACE INTO main.user_annotation_set (TUNE_REF, NotePrivate, NotePublic, Tags, USER_REF)
-SELECT TUNE_REF, NotePrivate, NotePublic, Tags, USER_REF
+INSERT OR REPLACE INTO main.user_annotation_set (tune_ref, note_private, note_public, tags, user_ref)
+SELECT CAST(TUNE_REF AS INTEGER), NotePrivate, NotePublic, Tags, USER_REF
 FROM source_db.user_annotation_set;
 
 -- Detach the source database after migration is done
