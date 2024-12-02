ca27b1390dd0813a48cf9ae859c89461899afd52
Scott Boag <sboag@gmail.com>
2024-12-01 21:57:48 -0500

    ✨ Turn All grid into Catalog grid, and attempt to add tunes to playlist.  WIP!

---
+++ frontend/app/(main)/pages/practice/components/tab_spec.ts
@@ -0,0 +1,29 @@
+import type { ITabSpec } from "@/app/(main)/pages/practice/components/TabsStateContext";
+
+export const initialTabSpec: ITabSpec[] = [
+  {
+    id: "scheduled",
+    name: "Practice",
+    content: "Review and practice your scheduled tunes.",
+    visible: true,
+  },
+  {
+    id: "repertoire",
+    name: "Repertoire",
+    content: "Manage your repertoire.",
+    visible: true,
+  },
+  {
+    id: "all",
+    name: "Catalog",
+    content:
+      "View full tune catalog, and add tunes from catalog to repertoire.",
+    visible: true,
+  },
+  {
+    id: "analysis",
+    name: "Analysis",
+    content: "Practice Analytics.",
+    visible: true,
+  },
+];
