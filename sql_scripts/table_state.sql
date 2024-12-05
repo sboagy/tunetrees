f5657d7e6d8f4076ef6648ce68f828e41929b5e7
Scott Boag <sboag@gmail.com>
2024-12-04 20:55:20 -0500

    🗃️  Update table_state_temp schema to include playlist_id in the primary key and adjust data migration accordingly.

--- sql_scripts/table_state.sql
+++ sql_scripts/table_state.sql
@@ -13,21 +13,25 @@
 --     check (screen_size IN ('small', 'full'))
 -- );
 
-CREATE TABLE table_state_temp (
-    user_id INTEGER NOT NULL REFERENCES user,
-    screen_size TEXT NOT NULL,
-    purpose TEXT NOT NULL,
-    settings TEXT,
-    current_tune INTEGER DEFAULT NULL,
-    playlist_id INTEGER NOT NULL REFERENCES playlist,
-    PRIMARY KEY (user_id, screen_size, purpose) ON CONFLICT REPLACE,
-    CHECK (purpose IN ('practice', 'repertoire', 'catalog', 'analysis')),
-    CHECK (screen_size IN ('small', 'full'))
+create table table_state_temp
+(
+    user_id      INTEGER not null
+        references user,
+    screen_size  TEXT    not null,
+    purpose      TEXT    not null,
+    settings     TEXT,
+    current_tune integer default null,
+    playlist_id  integer not null
+        references playlist,
+    constraint compound_primary_key
+        primary key (user_id, screen_size, purpose, playlist_id) on conflict replace,
+    check (purpose IN ('practice', 'repertoire', 'catalog', 'analysis')),
+    check (screen_size IN ('small', 'full'))
 );
 
 -- Copy data from the original table to the temporary table
 INSERT INTO table_state_temp
-SELECT user_id, screen_size, purpose, settings, current_tune, 1 FROM table_state;
+SELECT user_id, screen_size, purpose, settings, current_tune, playlist_id FROM table_state;
 
 -- Drop the original table
 DROP TABLE table_state;
