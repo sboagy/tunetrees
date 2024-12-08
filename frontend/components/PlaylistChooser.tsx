e09bda38453573f6ba5542aab8df0cb19e0d5edd
Scott Boag <sboag@gmail.com>
2024-12-08 10:35:37 -0500

    🚸  Update label in PlaylistChooser component from "Repertoire" to "Instrument"

--- frontend/components/PlaylistChooser.tsx
+++ frontend/components/PlaylistChooser.tsx
@@ -264,7 +264,7 @@ export default function PlaylistChooser() {
           className="px-4 py-2 border rounded bg-white dark:bg-background hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white-500 transition duration-150 ease-in-out flex items-center space-x-2"
           title={currentPlaylistDescription} // Add hover text
         >
-          <span>Repertoire: {currentPlaylistName}</span>
+          <span>Instrument: {currentPlaylistName}</span>
           <ChevronDownIcon className="w-5 h-5 text-gray-500" />
         </DropdownMenuTrigger>
         <DropdownMenuContent className="bg-white dark:bg-background border border-gray-300 dark:border-gray-700 rounded shadow-lg">
