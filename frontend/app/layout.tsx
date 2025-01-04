7e96db1aec14504115c550c69443cf4df5367837
Scott Boag <sboag@gmail.com>
2025-01-03 22:37:50 -0500

    ✨ Update default theme to dark in RootLayout component (temporary until I get a better Light mode)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
       <body className={`${inter.className} h-full`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
