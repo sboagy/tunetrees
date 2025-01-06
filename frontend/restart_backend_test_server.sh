34a7a5e797faf1caab86d4d6ad5cca3ec9de9f3f
Scott Boag <sboag@gmail.com>
2025-01-05 20:08:18 -0500

    ♻️ minor cleanup

--- frontend/restart_backend_test_server.sh
+++ frontend/restart_backend_test_server.sh
@@ -10,8 +10,6 @@ npx playwright test --project=backend
 
 sleep 1
 
-curl -X 'GET' 'http://localhost:8000/hello/bozo' -w "%{http_code}" -H 'accept: application/json'
-
 response=$(curl -w "%{http_code}" -o /dev/null -s -X 'GET' 'http://localhost:8000/hello/testFromScript')
 if [ "$response" -ne 200 ]; then
   echo "Backend is not available. HTTP status code: $response"
@@ -19,11 +17,6 @@ if [ "$response" -ne 200 ]; then
 else
   echo "Backend is available. HTTP status code: $response"
 fi
-
-# curl -w "%{http_code}" -o /dev/null -s -X 'GET' 'http://localhost:8000/hello/foo'
-# curl -w "%{http_code}" -o /dev/null -s -X 'GET' 'http://localhost:8000/hello/baz'
-# curl -w "%{http_code}" -o /dev/null -s -X 'GET' 'http://127.0.0.1:8000/hello/yada'
-# curl -w "%{http_code}" -o /dev/null -s -X 'GET' 'http://127.0.0.1:8000/hello/yodo'
 
 lsof -i :3000
 lsof -i :8000
