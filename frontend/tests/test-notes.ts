27f2826ed505ad39946939758abfc1fe0be78432
Scott Boag <sboag@gmail.com>
2025-01-13 23:20:21 -0500

    ✅   Assert note count after addition, and check note contents.

--- frontend/tests/test-notes.ts
+++ frontend/tests/test-notes.ts
@@ -42,6 +42,14 @@ test("test-notes-1", async ({ page }) => {
   const currentTuneTitleLocator = page.getByTestId("current-tune-title");
   await expect(currentTuneTitleLocator).toHaveText("Peacock's Feather");
 
+  const notesContentLocator = page.getByTestId("tt-notes-content");
+  await expect(notesContentLocator).toBeVisible();
+
+  const notesCount = await notesContentLocator.evaluate(
+    (node) => node.children.length,
+  );
+  console.log(`Number of notes: ${notesCount}`);
+
   const newNoteLocator = page.getByLabel("New Note");
   await expect(newNoteLocator).toBeVisible();
   await expect(newNoteLocator).toBeEnabled();
@@ -53,14 +61,66 @@ test("test-notes-1", async ({ page }) => {
   await expect(noteEditBoxLocator).toBeEditable();
   await noteEditBoxLocator.click();
 
-  await noteEditBoxLocator.pressSequentially("abcdef", { delay: 100 });
+  const newNoteText = "abcdef";
+
+  await noteEditBoxLocator.pressSequentially(newNoteText, { delay: 100 });
 
   const saveEditButtonLocator = page.getByTestId("tt-save-note");
   await expect(saveEditButtonLocator).toBeVisible();
   await expect(saveEditButtonLocator).toBeEnabled();
+
+  const responsePromise = page.waitForResponse(
+    (response) =>
+      response.url() === "https://localhost:3000/home" &&
+      response.status() === 200 &&
+      response.request().method() === "POST",
+  );
   await saveEditButtonLocator.click();
+  await responsePromise;
 
   console.log("===> test-notes.ts:38 ~ exit test-notes-1");
+
+  const notesContentLocator2 = page.getByTestId("tt-notes-content");
+  await expect(notesContentLocator2).toBeVisible();
+
+  const notesCount2 = await notesContentLocator2.evaluate(
+    (node) => node.children.length,
+  );
+  console.log(`Number of notes after add: ${notesCount2}`);
+
+  // Assert the count using evaluate
+  await expect(
+    notesContentLocator2.evaluate((node) => node.children.length),
+  ).resolves.toBe(notesCount + 1);
+
+  // "tt-note-text"
+  const firstNoteLocator = notesContentLocator2.locator(
+    ":scope > :first-child",
+  );
+  await expect(firstNoteLocator).toBeVisible();
+
+  const editorFieldLocator1 = firstNoteLocator.getByText(newNoteText);
+  await expect(editorFieldLocator1).toBeVisible();
+
+  const secondNoteLocator = notesContentLocator2.locator(
+    ":scope > :nth-child(2)",
+  );
+  await expect(secondNoteLocator).toBeVisible();
+
+  const editorFieldLocator2 = secondNoteLocator.getByText(
+    "2017-06-01: worked on the",
+  );
+  await expect(editorFieldLocator2).toBeVisible();
+
+  const thirdNoteLocator = notesContentLocator2.locator(
+    ":scope > :nth-child(3)",
+  );
+  await expect(thirdNoteLocator).toBeVisible();
+
+  const editorFieldLocator3 = thirdNoteLocator.getByText(
+    "https://www.youtube.com/watch",
+  );
+  await expect(editorFieldLocator3).toBeVisible();
 
   // await page.waitForTimeout(1000 * 100);
 });
