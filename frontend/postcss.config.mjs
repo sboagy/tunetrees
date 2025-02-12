35ebd83e3af00ac1360e420b663b74b1ad8b0a2d
Scott Boag <sboag@gmail.com>
2025-02-12 17:23:07 -0500

    🚧 ✨ Update PostCSS configuration and dependencies

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,6 +1,8 @@
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "postcss-import": {},
+    "postcss-nesting": {},
+    "@tailwindcss/postcss": {},
+    "autoprefixer": {},
   },
 };
