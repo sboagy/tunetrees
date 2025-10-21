38b7f804029c0f5799030762ac43b0c2b94b0bf0
Scott Boag <sboagy@gmail.com>
2025-10-21 09:33:13 -0400

    🔨 Make the delete_workflow_runs.sh skip count a script argument that defaults to 2

--- scripts/delete_workflow_runs.sh
+++ scripts/delete_workflow_runs.sh
@@ -7,8 +7,9 @@ fi
 
 OWNER="${GITT_OWNER:-sboagy}"
 REPO="${GITHUB_REPO:-tunetrees}"
+LIMIT="${1:-2}"
 
-echo "Deleting all workflow runs for $OWNER/$REPO"
+echo "Deleting all workflow runs for $OWNER/$REPO (skip first $LIMIT runs)"
 
 # Fetch all workflow runs
 workflow_runs=$(gh api -X GET /repos/$OWNER/$REPO/actions/runs --paginate -q '.workflow_runs[].id')
@@ -16,10 +17,9 @@ workflow_runs=$(gh api -X GET /repos/$OWNER/$REPO/actions/runs --paginate -q '.w
 # Delete each workflow run
 count=0
 for run_id in $workflow_runs; do
-  if [ $count -ge 1 ]; then
-    gh api -X DELETE /repos/$OWNER/$REPO/actions/runs/$run_id --silent
+  if [ "$count" -ge "$LIMIT" ]; then
+    gh api -X DELETE /repos/$OWNER/$REPO/actions/runs/"$run_id" --silent
     echo "Deleted workflow run: $run_id, count: $count"
-    # echo "count: $count; gh api -X DELETE /repos/$OWNER/$REPO/actions/runs/$run_id --silent"
   fi
   count=$((count + 1))
 done
