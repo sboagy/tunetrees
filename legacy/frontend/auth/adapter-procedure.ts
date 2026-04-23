2e2d53d92bc17c85ab871d05a3975b0932264d19
Scott Boag <sboagy@gmail.com>
2026-04-23 08:35:26 -0400

    ⚰️ Remove legacy folder!

--- legacy/frontend/auth/adapter-procedure.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export interface IAdapterProcedure {
-  path: string;
-  method: string;
-  body?: object;
-
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  select?: (res: any) => unknown;
-}
--- legacy/frontend/test-scripts/run-login.mts
+++
@@ -1,9 +0,0 @@
-import { globalSetup } from "./login.mts";
-
-try {
-  console.log("Starting global setup...");
-  await globalSetup();
-  console.log("Global setup completed.");
-} catch (error) {
-  console.error("Error during global setup:", error);
-}
